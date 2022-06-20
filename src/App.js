2c20b2bcf2739ab436a082a21fc43a8f84f096f2
bl1tz <Elbar.israilov@gmail.com>
2022-06-20 16:26:57 +0600

    commit friday

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Header from "./components/Header/Header";
+import Pokemons from "./components/Pokemons/Pokemons";
+import Pokemons2 from "./components/Pokemons2/Pokemons2";
+import RickAndMorty from "./components/RickAndMorty/RickAndMorty";
+import AddProduct from "./components/AddProduct/AddProduct";
+import Users from "./components/Users/Users";
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // указываем что роутинг будет в браузере
+    <BrowserRouter>
+      {/* header будет на всех наших страницах */}
+      <Header />
+      {/* для перечисления роутов */}
+      <Routes>
+        {/* непосредственно сами роуты */}
+        <Route path="/" element={<h1>Home Page</h1>} />
+        <Route path="/pokemons" element={<Pokemons />} />
+        <Route path="/pokemons-2" element={<Pokemons2 />} />
+        <Route path="/rick-and-morty" element={<RickAndMorty />} />
+        <Route path="/add" element={<AddProduct />} />
+      </Routes>
+      {/* footer будет на всех страницах */}
+    </BrowserRouter>
+
+    // <div>
+    //   {/* <Header /> */}
+    //   {/* <Pokemons /> */}
+    //   {/* <Pokemons2 />
+    //   <RickAndMorty />
+    //   <Users /> */}
+    // </div>
   );
-}
+};
 
 export default App;
